6306f4832167d3c9b367a7fe5346fd594584a3ee
duanyuxiang <duanyx@belink.com>
2020-06-11 08:57:19 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/demo1002.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo1002.dir/main.c.obj"
+  "demo1002.exe"
+  "demo1002.exe.manifest"
+  "demo1002.pdb"
+  "libdemo1002.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo1002.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
